ce5557017bbc2a232a149f787aed0244ae8a815e
yejikim9629 <itsds@naver.com>
2022-08-18 18:12:57 +0900

    start

---
+++ 22.js
@@ -0,0 +1,13 @@
+const variable =require('./variable.js')
+
+function checkNumberOddOrEveni(num){
+    if (num % 2){
+        return odd
+    }
+    return even
+}
+
+function checkNumberOddOrEveni(str)
+    if (str.length % 2)
+        return odd
+        
---
+++ ffdf.js
@@ -0,0 +1,7 @@
+const { sum, sub, mul, div, rem } = require("./play.js");
+
+console.log(sum(1, 2));
+console.log(sub(10, 5));
+console.log(mul(2, 10));
+console.log(div(10, 2));
+console.log(rem(10, 2));
